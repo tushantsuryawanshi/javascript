ac2379da721d9f41fd1ba173a31b937c09635f05
tushantsuryawanshi <tushantsuryawanshi159@gmail.com>
2025-02-28 19:54:32 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)// we are using node.js, not browser
+
+let age =18
+let name =  "tushant"
+let isLoggedin= false
+let state;
+// number
+// bigint
+//boolean
+//null=> standalone 
+//undefined/symbol
+//object
+console.log(typeof null)
