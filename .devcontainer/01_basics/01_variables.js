82c51c10bf64073b36fc4126e27ef55fd5ef7194
tushantsuryawanshi <tushantsuryawanshi159@gmail.com>
2025-03-01 14:58:51 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "tushantsuryawanshi159@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+accountEmail = "pn23@gmail.com"
+accountPassword = "21212121"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ .devcontainer/03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abs"+>Nav
+// true >1 false>00
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false
+// "" =>false
+//"tushant"=> ture
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
