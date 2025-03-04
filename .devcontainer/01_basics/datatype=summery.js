fa18766c887ba41d3c782badc2b644235c2ed9f1
tushantsuryawanshi <tushantsuryawanshi159@gmail.com>
2025-03-04 18:58:40 +0000

    done

---
+++ .devcontainer/01_basics/datatype=summery.js
@@ -0,0 +1,49 @@
+//# Primitive
+
+// 7 types : String , numbers,boolean,null,undefined,symbol,BigInt,
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn =false
+const outsideTemp =null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+//const bigNumber = 233423742547234n
+
+// Referance(non Primitive)
+
+// Array,object,function
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name:"Tushant",
+    age:22
+
+}
+const myFunction = function (){
+    console.log("Hello World ");
+    
+}
+console.log(typeof bigNumber);
+// https://262.ecma-interntional.org/5.1/#sec-11.4.3
+
+
+
+
+
+
+// ***********************************
+
+// Stack (Primitive), Heap(non-Primitive)
+
+let myYoutubename ="TushantSuryawanshi"
+
+let anothername ="myYoutubename"
+
+console.log(anothername);
